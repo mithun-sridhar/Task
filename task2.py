4299684d636e3447ed6e96293f6b4f1e13541c3a
mithun-sridhar <65328130+mithun-sridhar@users.noreply.github.com>
2020-05-14 12:01:20 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 11:59:53 2020
+
+@author: Mithun S
+"""
+
+
+fn=input('Enter filename: ')
+f=fn.split('.')
+print('Extension of the file is : '+f[-1])
